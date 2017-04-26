412657798a1ee5a85f22d544ebbfb83866ed4cde
helsingVan <it.fan@foxmail.com>
2017-04-26 21:46:33 +0800

    jquery

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -23,8 +23,14 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src')
+      // 'jquery': resolve('node_modules/jquery/dist/jquery.js')
     }
   },
+  // plugins: [
+  //   new webpack.ProvidePlugin({
+  //     $: 'jquery'
+  //   })
+  // ],
   module: {
     rules: [
       {
