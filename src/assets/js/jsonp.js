7212b88308d8e7f24042758ac1318224c483b0f1
helsingvan <it.fan@foxmail.com>
2017-09-03 11:37:47 +0800

    delete

--- src/assets/js/jsonp.js
+++
@@ -1,8 +0,0 @@
-export function jsonp(url) {
-	var script = document.createElement('script');
-	script.src = url+'?callback=callJson';
-	document.getElementsByTagName('head')[0].appendChild(script);
-	
-
-}
-
