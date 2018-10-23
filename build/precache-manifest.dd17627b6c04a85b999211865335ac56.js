e439c54e44f7c95e860f6b47e66144fb64775501
helsingvan <it.fan@foxmail.com>
2018-10-23 15:14:19 +0800

    build

---
+++ build/precache-manifest.dd17627b6c04a85b999211865335ac56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f87733292784802a7d9a",
+    "url": "/static/js/main.f8773329.chunk.js"
+  },
+  {
+    "revision": "394c4910b559d4d10fd5",
+    "url": "/static/js/1.394c4910.chunk.js"
+  },
+  {
+    "revision": "f87733292784802a7d9a",
+    "url": "/static/css/main.dbc04784.chunk.css"
+  },
+  {
+    "revision": "0a964c61d5a05300d51fdee0ac7d7027",
+    "url": "/index.html"
+  }
+];
